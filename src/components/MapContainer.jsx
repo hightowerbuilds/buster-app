38e5f866e4213c1bbf336eead5969fe84bdd2b83
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-02-20 19:56:01 -0800

    feb20.2

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -2,15 +2,9 @@ import { APIProvider, Map } from "@vis.gl/react-google-maps"
 
 export default function MapContainer() {
   return (
-    <div style={{ 
-      
-        height: '100vh',
-        width: '100%'
-    }}>
-       
+    <div style={{ height: '100vh', width: '100%' }}> 
     <APIProvider apiKey={import.meta.env.VITE_KEY}>
-        <Map
-  
+    <Map
       defaultCenter={{ lat: 47, lng: -122}}
       defaultZoom={10}
       gestureHandling={'greedy'}
