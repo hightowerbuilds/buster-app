42870ebfc536d518294a0c4ebd126431d1f551fd
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-02-21 18:41:00 -0800

    feb21.1

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -10,7 +10,23 @@ export default function MapContainer() {
       gestureHandling={'greedy'}
       disableDefaultUI={true}
     >
-   
+   <div style={{
+    position: 'absolute',
+    zIndex: 0,
+    height: '95vh',
+    width: 300,
+    border: '3px black solid',
+    marginTop: 15,
+    marginLeft: 15,
+    padding: 5,
+    backdropFilter: 'blur(5px)',
+    fontFamily: 'monospace'
+
+   }}>
+   <h2>DIRECTIONS</h2>
+   <p>start address</p><input type="text"/>
+   <p>end address</p> <input type="text" />
+   </div>
  
     </Map>
     </APIProvider>
