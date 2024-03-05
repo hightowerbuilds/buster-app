1cc48a2516d5cf3237d54d0be6ea1a289ed54a44
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-03-04 19:56:24 -0800

    mar3.2

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,11 +7,36 @@ export default function Dashboard() {
         width: '100%',
         backgroundColor: '#242424'
     }}>
+        {/** NAV BAR... needs work  */}
         <div>
             <Link to="/" style={{ color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>back to map container</Link>
-                
+            
         </div>
-        
+
+
+        {/** dashboard cluster */}
+        <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+            history of locations
+        </div>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+            ride distances
+        </div>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+            payouts per ride 
+        </div>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+           current events 
+        </div>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+            pick-up notes 
+        </div>
+        <div style={{ height: 500, width: 500, border: '2px beige solid', borderRadius: 5, color: 'beige', fontFamily: 'monospace', fontSize: 15, }}>
+            ratings and reviews
+        </div>
+
+        </div>
+
        
     </div>
   )
