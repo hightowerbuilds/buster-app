b553edd7ba91d6f51cc6afd1d2174fda2322ae05
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-03-10 11:41:07 -0700

    march10.1

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+	<Provider store={store}>
+	<React.StrictMode>
 	<BrowserRouter>
 	<App />
 	</BrowserRouter >
- </React.StrictMode>
+	</React.StrictMode>
+	</Provider>
+	
+ 
 )
